e0659233bb2f7e5fdea441917c8a09d59a45ab67
unknown <business.life.bekir@gmail.com>
2025-01-06 14:06:37 +0100

    problem

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { Firestore, collection, query, where, getDocs } from '@angular/fire/firestore';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { PrdctList } from '../models/prdct-list';
 import { Route, Router } from '@angular/router';
 import { AddProductComponent } from '../add-product/add-product.component';
+import { ProductService } from '../service/product.service';
+
+
 
 
 @Component({
@@ -15,6 +18,35 @@ import { AddProductComponent } from '../add-product/add-product.component';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent  {
+export class ProductListComponent implements OnInit {
+  
+  products: any[] = [];
+  products$ = this.productService.getProducts('drinks', 'softdrinks');
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts('drinks', 'softdrinks').subscribe({
+      next: (data) => {
+        this.products = data; // Produkte laden
+        console.log('Geladene Produkte:', this.products);
+      },
+      error: (err) => {
+        console.error('Fehler beim Laden der Produkte:', err);
+      },
+    });
+  }
+
+
+  addProduct() {
+    const product = {
+      name: 'Coca-Cola',
+      price: 1.2,
+      stockQuantity: 50,
+      expirationDate: '2025-01-01',
+    };
+    this.productService.addProduct('drinks', 'softdrinks', 'coca-cola', product).then(() => {
+      console.log('Produkt hinzugefügt!');
+    });
+  }
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,17 +1,44 @@
 import { Injectable } from '@angular/core';
-import { PrdctList } from '../models/prdct-list';
-import { deleteDoc, doc } from 'firebase/firestore';
+import { Firestore, doc, setDoc, collection, collectionData } from '@angular/fire/firestore';
+import { getDocs, query } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
  
+  constructor(private firestore: Firestore) {}
 
+  // Kategorie hinzufügen
+  async addCategory(categoryId: string, categoryName: string) {
+    const categoryDoc = doc(this.firestore, `products/${categoryId}`);
+    await setDoc(categoryDoc, { category: categoryName });
+  }
 
-  
+  // Unterkategorie hinzufügen
+  async addSubcategory(categoryId: string, subcategoryId: string, subcategoryName: string) {
+    const subcategoryDoc = doc(this.firestore, `products/${categoryId}/subcategory/${subcategoryId}`);
+    await setDoc(subcategoryDoc, { subcategory: subcategoryName });
+  }
 
+  // Produkt hinzufügen
+  async addProduct(categoryId: string, subcategoryId: string, productId: string, productData: any) {
+    const productDoc = doc(
+      this.firestore,
+      `products/${categoryId}/subcategory/${subcategoryId}/product/${productId}`
+    );
+    await setDoc(productDoc, productData);
+  }
 
-
-
+  // Produkte abrufen
+  getProducts(categoryId: string, subcategoryId: string): Observable<any[]> {
+    const productsRef = collection(
+      this.firestore,
+      `products/${categoryId}/subcategory/${subcategoryId}/product`
+    ); // Referenz zur Unterkollektion
+    return collectionData(productsRef, { idField: 'id' }); // Ruft die Daten direkt ab
+  }
 }
+  
